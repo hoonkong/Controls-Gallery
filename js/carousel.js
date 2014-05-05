f41bdcc4115c00062df72c5abfb6c5be9c77d015
Hoon <hoonkong@gmail.com>
2014-05-05 01:42:08 -0700

    Updated form processor and carousel control

---
+++ js/carousel.js
@@ -0,0 +1,23 @@
+var Carousel;
+
+(function (Carousel) {
+	
+	var init = function () {
+		var data = {
+			myCarousel: [
+				"Item 1",
+				"Item 2",
+				"Item 3",
+				"Item 4",
+				"Item 5",
+				"Item 6",
+				"Item 7",
+				"Item 8",
+				"Item 9",
+				"Item 10",
+				"Item 11",
+				"Item 12",
+			]
+		}
+	}
+})(Carousel || (Carousel = {}));
--- js/formprocessor.js
+++ js/formprocessor.js
@@ -1,32 +1,54 @@
 var FormProcessor;
 
 (function (FormProcessor) {
-	function Processor(formId) {
+	var Processor;
+	(function (Processor) {
+		var formInfo;
+
 		var processSelects = function (form) {
-			var selects = form.getElementByTags("select");
+			var selects = form.getElementsByTagName("select");
+
+			for (var i = 0; i < selects.length; i++) {
+				var options = selects[i].options;
+
+				if (options.length) {
+					var name = selects[i].getAttribute("name");
+					formInfo[name] = [];
+					for (var j = 0; j < options.length; j++) {
+						options[j].selected && formInfo[name].push(options[j].value);
+					}
+				}
+			}
+
+			console.log(formInfo);
 		}
 
 		var processSubmit = function (evtArgs) {
 			evtArgs.preventDefault();
 
 			var form = evtArgs.target;
+			formInfo = {};
 			processSelects(form);
 
 			return false;
 		};
 
-		var form = document.getElementById(formId);
-		form.addEventListener("submit", processSubmit);
-	}
+		//var form = document.getElementById(formId);
+		//form.addEventListener("submit", processSubmit);
 
-	var processors;
+		Processor.processSubmit = processSubmit;
+	})(Processor || (Processor = {}));
 
 	var init = function (formId) {
-		processors = {};
-		processors[formId] = new Processor(formId);
+		//processors = {};
+		//processors[formId] = new Processor(formId);
+
+		var form = document.getElementById(formId);
+		form.addEventListener("submit", Processor.processSubmit);
 	}
 
 	FormProcessor.init = init;
 })(FormProcessor || (FormProcessor = {}));
 
 FormProcessor.init("myFrom");
+FormProcessor.init("myFrom1");
