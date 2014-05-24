3fb15e0875dd2e17032bca099b001076a6dddc29
hoonkong <hoonkong@gmail.com>
2014-05-24 13:13:42 -0700

    Updated carousel control

--- js/carousel.js
+++ js/carousel.js
@@ -1,23 +1,50 @@
 var Carousel;
 
 (function (Carousel) {
-	
-	var init = function () {
-		var data = {
-			myCarousel: [
-				"Item 1",
-				"Item 2",
-				"Item 3",
-				"Item 4",
-				"Item 5",
-				"Item 6",
-				"Item 7",
-				"Item 8",
-				"Item 9",
-				"Item 10",
-				"Item 11",
-				"Item 12",
-			]
+	var createCardContent = function (cardContainer, data) {
+		var title = document.createElement("p");
+		title.appendChild(document.createTextNode(data.title));
+		var img = document.createElement("img");
+		img.src = data.imgUrl;
+		var desc = document.createElement("p");
+		desc.appendChild(document.createTextNode(data.description));
+		cardContainer.appendChild(title);
+		cardContainer.appendChild(img);
+		cardContainer.appendChild(desc);
+	}
+
+	var render = function (container, data) {
+		if (!data) {
+			return;
 		}
+
+		var card = document.createElement("div");
+		if (data.content) {
+			card.innerHtml = data.content;
+		} else {
+			createCardContent(card, data);
+		}
+
+		container.appendChild(card);
+	}
+
+	var init = function () {
+		var controls = document.querySelectorAll("data-control");
+		for (var i = 0; i < controls.length; i++) {
+			var controlName = controls[i].getAttribute("data-control");
+			if (controlName.toLowerCase() === "carousel") {
+				var carouselDataStr = controls[i].getAttribute("data-carousel");
+				var carouselDataObj = JSON.parse(carouselDataStr);
+				render(controls[i], carouselDataObj);
+			}
+		}
+	}
+
+	var showNext = function () {
+
+	}
+
+	var showPrev = function () {
+
 	}
 })(Carousel || (Carousel = {}));
